17cceca8597ec0ff79c84e3ee3f5b8a10476c07a
adesai90 <adesai.physics@gmail.com>
2026-04-27 13:18:55 -0400

    Updated branch

--- setup_package_test.sh
+++ setup_package_test.sh
@@ -60,8 +60,10 @@ if [ "$answer" = "1" ]; then
 
     # FIX ROOT to make sure glast root is used!
     export ROOTSYS=/sdf/group/fermi/a/ground/GLAST_EXT/redhat6-x86_64-64bit-gcc44/ROOT/v5.26.00
-    export PATH=$ROOTSYS/bin:$PATH
+    export PATH=$ROOTSYS/bin:$(echo $PATH | tr ':' '\n' | grep -v miniconda | grep -v conda | tr '\n' ':')
     export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+    unset CONDA_PREFIX_ROOT
+    unset ROOTSYS_CONDA
 
     hash -r
 
